59c3ab6d16e00baa70fe5c9d943705fd6d0811c4
henryhamer <71504222+hentity@users.noreply.github.com>
2025-05-02 23:29:33 +1000

    deploy attempt with frontend as root

--- frontend/src/components/after/After.tsx
+++ frontend/src/components/after/After.tsx
@@ -3,9 +3,6 @@ import LessonPanelAfter from './LessonPanelAfter'
 export default function After() {
   return (
     <>
-      {/* <div className="h-full flex justify-right">
-        <NotePanelAfter />
-      </div> */}
       <div className="h-full flex justify-center">
         <LessonPanelAfter />
       </div>
