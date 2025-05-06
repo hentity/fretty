66c0e52f3667fe717ee4aec2b32026d633de2c96
hentity <hhame4@gmail.com>
2025-05-06 17:47:34 +1000

    redeploy

--- frontend/src/components/after/After.tsx
+++ frontend/src/components/after/After.tsx
@@ -3,7 +3,7 @@ import LessonPanelAfter from './LessonPanelAfter'
 export default function After() {
   return (
     <>
-      <div className="h-full flex justify-center">
+      <div className="h-full flex justify-center ">
         <LessonPanelAfter />
       </div>
     </>
